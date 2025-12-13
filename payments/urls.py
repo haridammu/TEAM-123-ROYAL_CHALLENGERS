a0f4c1fa4001a42681d3d2b89b166ef350fd212e
Gundumogula Dhana Sai <saigundumogula5@gmail.com>
2025-12-13 15:52:05 +0530

    Delete payments directory

--- payments/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rest_framework.routers import DefaultRouter
-from . import views
-
-urlpatterns = [
-    path('plans/', views.subscription_plans_list, name='subscription_plans_list'),
-    path('subscriptions/', views.user_subscriptions_list, name='user_subscriptions_list'),
-    path('subscribe/', views.subscribe_to_plan, name='subscribe_to_plan'),
-]
