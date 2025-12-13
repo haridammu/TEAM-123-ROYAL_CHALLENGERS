d7e9a1304cfc2a78a670e55ddca14da2fe45ac4c
Gundumogula Dhana Sai <saigundumogula5@gmail.com>
2025-12-13 15:49:31 +0530

    Delete LEARNING MANAGEMENT SYSTEM/code_executor_service directory

--- LEARNING MANAGEMENT SYSTEM/code_executor_service/execution/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import CodeSnippet
-
-class CodeSnippetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CodeSnippet
-        fields = ['id', 'language', 'title', 'code', 'output', 'created_at', 'updated_at']
-        read_only_fields = ['user', 'output', 'created_at', 'updated_at']
